78babc9135e5bacb3ae4d74f0259a03b694dfcd3
Jon Boldt <dssjon@gmail.com>
2023-11-16 15:22:38 -0800

    target book chapter test

--- test/test.py
+++ test/test.py
@@ -49,35 +49,31 @@ def get_count_of_words_in_db(db):
         total_word_count += len(item.split())
     return total_word_count
 
-def query_databases(dbs, queries):
-
+def query_databases(dbs, test_queries):
     results = []
     for idx, db in enumerate(dbs):
         db_results = []
-        for query in queries:
-
-            # TODO: configure k and score function
+        for query, validating_verses in test_queries.items():
             search_results = db.similarity_search_with_relevance_scores(
                 query, k=4, score_function="cosine"
             )
             query_words = query.lower().split()
 
-            page_contents = [doc[0].page_content.replace("\n", " ") for doc in search_results]
-
-            # TODO: Find semantic matches
-            match_count = sum(
-                any(word in content.lower() for word in query_words)
-                for content in page_contents
-            )
-            total_similarity_scores = sum(match[1] for match in search_results)
+            # Check if the returned verses match the expected validating verses
+            returned_verses = [f"{doc[0].metadata['book']} {doc[0].metadata['chapter']}" for doc in search_results]
+            print(returned_verses)
+            matches = sum(verse in validating_verses for verse in returned_verses)
+            print(matches)
             
+            total_similarity_scores = sum(match[1] for match in search_results)
 
             db_results.append(
                 {
                     "idx": idx,
                     "query": query,
-                    "matches": match_count,
-                    "search_results": search_results,
+                    "matches": matches,
+                    "validating_verses": validating_verses,
+                    "returned_verses": returned_verses,
                     "total_similarity_scores": total_similarity_scores,
                 }
             )
@@ -85,7 +81,7 @@ def query_databases(dbs, queries):
         total_matches = sum(result["matches"] for result in db_results)
         total_scores = sum(result["total_similarity_scores"] for result in db_results)
         total_word_count = get_count_of_words_in_db(db)
-        
+
         db_results.append(
             {
                 "idx": idx,
@@ -104,27 +100,26 @@ def get_chroma_db(embedding_function, persist_directory):
         persist_directory=persist_directory, embedding_function=embedding_function
     )
 
-
 def get_embedding_function(model_name, model_kwargs, encode_kwargs, embedding_class):
     return embedding_class(
         model_name=model_name, model_kwargs=model_kwargs, encode_kwargs=encode_kwargs
     )
 
-
 model_kwargs = {"device": "cpu"}
 encode_kwargs = {"normalize_embeddings": True}
 
-# TODO: Expand to include additional instructor models, with query instruction params, trained on varying instruction prompts
-# TODO: Allow for creation of db from scratch, instead of including pre-built dbs
+# Refined query instruction
+query_instruction = "Analyze and encapsulate the theological doctrines, historical context, narrative structure, and ethical teachings present in this biblical verse for in-depth semantic search and understanding:"
+
 db_configs = [
     {
-        # NOTE: This db was generated with a 100 character offset per chunk hence the overlapping added word count
         "model_name": "BAAI/bge-large-en-v1.5",
         "persist_directory": "./test_data/bge_large_1k_db",
         "embedding_class": HuggingFaceBgeEmbeddings,
     },
     {
         "model_name": "hkunlp/instructor-large",
+        "model_kwargs": {"query_instruction": query_instruction},  # Pass the refined query instruction
         "persist_directory": "./test_data/instructor_large_1K_db",
         "embedding_class": HuggingFaceInstructEmbeddings,
     },
@@ -140,16 +135,22 @@ dbs = [
     for config in db_configs
 ]
 
-# TODO: Add queries and implement better scoring considering the number semantic matches & target verses desired
-queries = [
-    "three days nights",
-    "Keys Kingdom Heaven",
-    "Sign Jonah",
-    "Doeg Edomite",
-    "What did Jesus say about eternal life?",
-]
+test_queries = {
+    "What did Jesus say about eternal life?": ["JHN 3", "JHN 17", "MAT 19"],
+    "What does the parable of the Prodigal Son reveal about forgiveness and family relationships?": ["LUK 15"],
+    "How is faith described in the New Testament?": ["HEB 11"],
+    "What are the Beatitudes and what do they teach about Christian life?": ["MAT 5"],
+    "What does Psalm 23 reflect about God's guidance and protection?": ["PSA 23"],
+    "How does the book of Revelation describe the end times?": ["REV 21", "REV 22"],
+    "What lessons can be learned from the story of David and Goliath?": ["1SA 17"],
+    "What does Paul say about love in his letters to the Corinthians?": ["1CO 13"],
+    "What teachings are given in the Sermon on the Mount?": ["MAT 5", "MAT 6", "MAT 7"],
+    "How does the book of Genesis describe the creation?": ["GEN 1", "GEN 2"]
+    # ... [add more test queries as needed]
+}
 
-results = query_databases(dbs, queries)
+
+results = query_databases(dbs, test_queries)
 
 st.title("Test results")
 
@@ -157,10 +158,9 @@ st.subheader(f"Total word count of engwebp_vpl_xml: {get_count_of_words_in_engwe
 
 columns = st.columns(len(dbs))
 for idx, col in enumerate(columns):
-    # TODO: Return db name and render instead of idx
     col.header(f"Database {idx + 1}")
 
-for query in queries + ["total"]:
+for query in list(test_queries.keys()) + ["total"]:
     for idx, col in enumerate(columns):
         res = next(
             (item for item in results if item["query"] == query and item["idx"] == idx),
