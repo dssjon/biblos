4785f911ff018510276cca922fc9b847b140dd4c
Jon Boldt <dssjon@gmail.com>
2023-11-20 08:59:05 -0800

    target book chapter test

--- test/test.py
+++ test/test.py
@@ -4,6 +4,7 @@ from langchain.schema import Document
 import xml.etree.ElementTree as ET
 import collections
 import streamlit as st
+import pandas as pd
 
 def get_count_of_words_in_engwebp_vpl_xml():
     tree = ET.parse("../data/engwebp_vpl.xml")
@@ -49,21 +50,21 @@ def get_count_of_words_in_db(db):
         total_word_count += len(item.split())
     return total_word_count
 
-def query_databases(dbs, test_queries):
+def query_databases(dbs, test_queries, k, score_function):
     results = []
     for idx, db in enumerate(dbs):
         db_results = []
         for query, validating_verses in test_queries.items():
             search_results = db.similarity_search_with_relevance_scores(
-                query, k=4, score_function="cosine"
+                query, k=k, score_function=score_function
             )
-            query_words = query.lower().split()
 
             # Check if the returned verses match the expected validating verses
             returned_verses = [f"{doc[0].metadata['book']} {doc[0].metadata['chapter']}" for doc in search_results]
-            print(returned_verses)
+            print("validating:" , validating_verses)
+            print("returned: ", returned_verses)
             matches = sum(verse in validating_verses for verse in returned_verses)
-            print(matches)
+            print("matches: ", matches)
             
             total_similarity_scores = sum(match[1] for match in search_results)
 
@@ -109,7 +110,11 @@ model_kwargs = {"device": "cpu"}
 encode_kwargs = {"normalize_embeddings": True}
 
 # Refined query instruction
-query_instruction = "Analyze and encapsulate the theological doctrines, historical context, narrative structure, and ethical teachings present in this biblical verse for in-depth semantic search and understanding:"
+#query_instruction = "Analyze and encapsulate the theological doctrines, historical context, narrative structure, and ethical teachings present in this biblical verse for in-depth semantic search and understanding:"
+query_instruction = "Represent the Religious question for retrieving related passages: "
+query_instruction2 = "Conduct a detailed semantic search to find biblical verses closely related to the theological, historical, and ethical aspects of the following query, focusing on direct scriptural relevance and doctrinal accuracy: "
+query_instruction3 = "Analyze and encapsulate the theological doctrines, historical context, narrative structure, and ethical teachings present in this biblical verse for in-depth semantic search and understanding:"
+query_instruction4 = "Represent the Religious Bible verse text for semantic search:"
 
 db_configs = [
     {
@@ -119,10 +124,36 @@ db_configs = [
     },
     {
         "model_name": "hkunlp/instructor-large",
-        "model_kwargs": {"query_instruction": query_instruction},  # Pass the refined query instruction
+        "model_kwargs": {"query_instruction": query_instruction},
+        # create_db -> query_instruction: "Represent the Religious passage for retrieval: "
+        "persist_directory": "./test_data/instructor_large_1K_db_new_instruction",
+        "embedding_class": HuggingFaceInstructEmbeddings,
+    },
+    {
+        "model_name": "hkunlp/instructor-large",
+        "model_kwargs": {"query_instruction": query_instruction}, 
         "persist_directory": "./test_data/instructor_large_1K_db",
         "embedding_class": HuggingFaceInstructEmbeddings,
     },
+    {
+        "model_name": "hkunlp/instructor-large",
+        "model_kwargs": {"query_instruction": query_instruction2}, 
+        "persist_directory": "./test_data/instructor_large_1K_db",
+        "embedding_class": HuggingFaceInstructEmbeddings,
+    },
+    {
+        "model_name": "hkunlp/instructor-large",
+        "model_kwargs": {"query_instruction": query_instruction3},
+        "persist_directory": "./test_data/instructor_large_1K_db",
+        "embedding_class": HuggingFaceInstructEmbeddings,
+    },
+        {
+        "model_name": "hkunlp/instructor-large",
+        "model_kwargs": {"query_instruction": query_instruction4},
+        "persist_directory": "./test_data/instructor_large_1K_db",
+        "embedding_class": HuggingFaceInstructEmbeddings,
+    },
+    
 ]
 
 dbs = [
@@ -138,10 +169,10 @@ dbs = [
 test_queries = {
     "What did Jesus say about eternal life?": ["JHN 3", "JHN 17", "MAT 19"],
     "What does the parable of the Prodigal Son reveal about forgiveness and family relationships?": ["LUK 15"],
-    "How is faith described in the New Testament?": ["HEB 11"],
+    "How is faith described in the New Testament?": ["HEB 11", "ROM 4", "JAS 2"],
     "What are the Beatitudes and what do they teach about Christian life?": ["MAT 5"],
     "What does Psalm 23 reflect about God's guidance and protection?": ["PSA 23"],
-    "How does the book of Revelation describe the end times?": ["REV 21", "REV 22"],
+    "How does the book of Revelation describe the end times?": ["REV 21", "REV 22", "REV 20"],
     "What lessons can be learned from the story of David and Goliath?": ["1SA 17"],
     "What does Paul say about love in his letters to the Corinthians?": ["1CO 13"],
     "What teachings are given in the Sermon on the Mount?": ["MAT 5", "MAT 6", "MAT 7"],
@@ -150,31 +181,36 @@ test_queries = {
 }
 
 
-results = query_databases(dbs, test_queries)
+results = query_databases(dbs, test_queries, 4, "cosine")
+# below has no effect on scoring
+#results2 = query_databases(dbs, test_queries, 4, "l2")
+#results3 = query_databases(dbs, test_queries, 4, "ip")
 
 st.title("Test results")
 
 st.subheader(f"Total word count of engwebp_vpl_xml: {get_count_of_words_in_engwebp_vpl_xml()}")
 
-columns = st.columns(len(dbs))
-for idx, col in enumerate(columns):
-    col.header(f"Database {idx + 1}")
+def results_to_dataframe(results):
+    data = {
+        "Database": [],
+        "Query": [],
+        "Validating Verses": [],
+        "Returned Verses": [],
+        "Matches": [],
+        "Total Similarity Scores": [],
+        "Total Word Count": []
+    }
 
-for query in list(test_queries.keys()) + ["total"]:
-    for idx, col in enumerate(columns):
-        res = next(
-            (item for item in results if item["query"] == query and item["idx"] == idx),
-            None,
-        )
-        col.write(f"Query: {query}")
-        col.write(f"Matches: {res['matches'] if res else 'N/A'}")
-        if res and "search_results" in res:
-            expander = col.expander("Search results")
-            expander.write(f"{res['search_results']}")
-        col.write(
-            f"Total Similarity Scores: {res['total_similarity_scores'] if res else 'N/A'}"
-        )
-        if res and "total_word_count" in res:
-            col.write(
-                f"Total Word Count: {res['total_word_count'] if res else 'N/A'}"
-            )
+    for result in results:
+        data["Database"].append(f"Database {result['idx'] + 1}")
+        data["Query"].append(result["query"])
+        data["Validating Verses"].append(", ".join(result.get("validating_verses", [])))
+        data["Returned Verses"].append(", ".join(result.get("returned_verses", [])))
+        data["Matches"].append(result.get("matches", 'N/A'))
+        data["Total Similarity Scores"].append(result.get("total_similarity_scores", 'N/A'))
+        data["Total Word Count"].append(result.get("total_word_count", 'N/A'))
+
+    return pd.DataFrame(data)
+
+df_results = results_to_dataframe(results)
+st.dataframe(df_results)
