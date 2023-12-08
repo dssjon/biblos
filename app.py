45ebc06188d9e469b8d0d0881d5ac2c92c3dca9e
Jon Boldt <dssjon@gmail.com>
2023-12-08 13:32:20 -0800

    Simplify

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceInstructEmbeddings
-from langchain.chat_models import ChatAnthropic
+#from langchain.chat_models import ChatAnthropic
 import streamlit_analytics
 import random
 from constants import *
@@ -9,7 +9,6 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 streamlit_analytics.start_tracking(load_from_json=ANALYTICS_JSON_PATH)
 
-# Set page config
 st.set_page_config(
     layout="wide",
     menu_items={
@@ -35,13 +34,6 @@ def setup_db(persist_directory, query_instruction):
 
 bible_db = setup_db(DB_DIR, DB_QUERY)
 commentary_db = setup_db(COMMENTARY_DB_DIR, COMMENTARY_DB_QUERY)
-
-# # Setup LLM
-# try:
-#     llm = ChatAnthropic(max_tokens=MAX_TOKENS, model_name=LLM_MODEL_NAME)
-# except:
-#     st.error(LLM_NOT_FOUND)
-#     llm = None
 
 st.title(TITLE)
 
@@ -73,43 +65,22 @@ def create_author_filters(church_fathers, columns):
 
 
 def select_options():
-    col1, col2, col3 = st.columns([1, 1, 1])
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
     with col1:
         ot = st.checkbox("Old Testament", value=True)
     with col2:
         nt = st.checkbox("New Testament", value=True)
     with col3:
         st.session_state.enable_commentary = st.checkbox("Church Fathers", value=False)
-    return ot, nt
+    with col4:
+        count = st.slider("Number of Bible Results", min_value=1, max_value=15, value=3, step=1)
+    return ot, nt, count
 
-
-def select_number_of_results(min_value, max_value, default_value, step):
-    with st.header("Number of Results"):
-        return st.slider(
-            "Number of bible passages to include:",
-            min_value=min_value,
-            max_value=max_value,
-            value=default_value,
-            step=step,
-        )
 
 
 with st.expander("Search Options"):
     
-    ot_checkbox, nt_checkbox = select_options()
-    num_verses_to_retrieve = select_number_of_results(1, 15, 3, 1)
-    
-    cols = st.columns(3)
-
-    # with cols[0]:
-    #     if st.button("Select all"):
-    #         update_author_selection(select_all=True)
-    #     if st.button("Remove all"):
-    #         update_author_selection(select_all=False)
-
-    #author_filters = create_author_filters(CHURCH_FATHERS, cols)
-
-    #st.session_state.enable_llm = st.checkbox("Enable LLM Summary", value=False)
+    ot_checkbox, nt_checkbox, count = select_options()
 
 def get_selected_bible_filters(ot, nt):
     if ot != nt:
@@ -119,7 +90,7 @@ def get_selected_bible_filters(ot, nt):
 
 bible_search_results = bible_db.similarity_search_with_relevance_scores(
     search_query,
-    k=num_verses_to_retrieve,
+    k=count,
     score_function=SCORE_FUNCTION,
     filter=get_selected_bible_filters(ot_checkbox, nt_checkbox),
 )
@@ -128,7 +99,6 @@ bible_search_results = bible_db.similarity_search_with_relevance_scores(
 def perform_commentary_search_parallel(commentary_db, search_query):
     search_results = []
 
-    # Define a function to perform the search for a single author
     def search_for_author(author):
         return commentary_db.similarity_search_with_relevance_scores(
             search_query,
@@ -196,30 +166,6 @@ def display_commentary_results(results):
                 if source_info:
                     st.write(f"**Source**: {source_info}")
                 st.write(SCORE_RESULT.format(value=score))
-    #formatted_commentary_results = format_commentary_results(commentary_results)
-    # llm_response = summarize_results(llm, formatted_commentary_results, COMMENTARY_SUMMARY_PROMPT)
-    # if llm_response:
-    #     st.success(llm_response)
-    # else:
-    #     st.error("No commentary results to summarize. Please load the results first.")
-
-
-
-# def summarize_results(llm, results, summary_prompt):
-#     if llm is None:
-#         st.error(LLM_ERROR)
-#         st.stop()
-#     else:
-#         with st.spinner("Summarizing..."):
-#             if not results:
-#                 st.error("No results found")
-#                 st.stop()
-
-#             llm_query = summary_prompt.format(
-#                 topic=search_query, content="\n".join(results)
-#             )
-#             return llm.predict(llm_query)
-
 
 def format_commentary_results(commentary_results):
     return [
@@ -237,15 +183,9 @@ def format_bible_results(bible_search_results):
 
 display_bible_results(bible_search_results)
 
-# if st.session_state.enable_llm:
-#     formatted_bible_results = format_bible_results(bible_search_results)
-#     llm_response = summarize_results(llm, formatted_bible_results, BIBLE_SUMMARY_PROMPT)
-#     if llm_response:
-#         st.success(llm_response)
 
 if st.session_state.enable_commentary:
    st.divider()
-   #selected_authors = [author for author, is_selected in author_filters.items() if is_selected]
    commentary_results = perform_commentary_search_parallel(commentary_db, search_query)
    display_commentary_results(commentary_results)
 
